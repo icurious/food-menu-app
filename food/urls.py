dbac23612a2c577b0917d331c45644dcfa3d7ef2
Dheeraj <kalwanidheeraj09@gmail.com>
2023-08-16 12:21:36 +0530

    Added view,migrations and detail item view.

--- food/urls.py
+++ food/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from food import views
 
 urlpatterns = [
+    # After url ---> /food/
     path('', views.index, name='index'),
+    path('<int:item_id>',views.detail, name='detail')
 ]
--- food/views.py
+++ food/views.py
@@ -1,6 +1,15 @@
+from re import template
 from django.shortcuts import render,HttpResponse
-
+from food.models import Item
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello')
+    item_list = Item.objects.all()
+    context = { 'item_list': item_list }
+    return render(request,'food/index.html',context)
+
+def detail(request,item_id):
+    item_detail = Item.objects.get(pk=item_id)
+    context = {'item_detail': item_detail}
+    return render(request,'food/detail.html',context)
